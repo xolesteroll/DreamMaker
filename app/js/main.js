ac00b553b709f3e6b9dd3c9cfa7a71e9f6c60de5
Anton <Iarovoi>
2020-09-30 14:58:52 +0300

    people section

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,8 @@ $(function() {
 
 });
 
-const provideItems = document.querySelectorAll('.provide__item');
+const provideItems = document.querySelectorAll('.provide__item'),
+    header = document.querySelector('header')
 
 provideItems.forEach(item => {
     item.addEventListener('click', () => {
@@ -11,4 +12,12 @@ provideItems.forEach(item => {
         });
         item.style.borderColor = '#9d9d9d';
     });
+});
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset >= 50) {
+        header.style.cssText = 'background-color: rgba(196,196,196,0.9);';
+    } else {
+        header.style.cssText = 'background-color: rgba(196,196,196,0.1);';
+    }
 });
