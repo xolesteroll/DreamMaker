7fe35cdeb43df1a5f5c38b6ebfd082ff0b93d400
Anton <Iarovoi>
2020-09-30 14:36:25 +0300

    second sectiion, click event with items

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function() {
 
 });
+
+const provideItems = document.querySelectorAll('.provide__item');
+
+provideItems.forEach(item => {
+    item.addEventListener('click', () => {
+        provideItems.forEach(item => {
+            item.style.borderColor = 'transparent';
+        });
+        item.style.borderColor = '#9d9d9d';
+    });
+});
